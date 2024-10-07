63d1a75af4aca08a2da6d7cd358e8123bcdb0d40
Elamparithi-Kannan <e1352457@u.nus.edu>
2024-10-08 02:57:48 +0800

    Create InsertScripts-coverage_cd.sql

---
+++ InsertScripts-coverage_cd.sql
@@ -0,0 +1,35 @@
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CL', 'CR', 'Car Collision', 'Collision into another car', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CL', 'OB', 'Object Collision', 'Collision into objects like tree or guardrail', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CH', 'TH', 'Theft', 'Car theft/ Car part theft', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CH', 'VN', 'Vandalism', 'Intentional destruction or damage to the car', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CH', 'ND', 'Natural disaster', 'Natural disasters like floods, hurricanes, earthquakes etc.,', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CH', 'FR', 'Fire', 'Car Fire', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CH', 'FL', 'Falling object', 'Falling objects like tree branches', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CH', 'AN', 'Animal Collision', 'Animal collision like hitting a deer', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('CH', 'GL', 'Glass Damage', 'Glass Damage like broken windshields', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('UN', 'UN', 'Uninsured Motorist', 'Protects the policyholder if they are hit by a driver who does not have insurance. It typically covers medical expenses, lost wages, and other injury-related costs.', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('MP', 'MP', 'Medical Payments', 'or the health costs of client and the passengers of the client’s vehicle, injured in an accident, regardless of whose fault', 'admin', 'admin');
+
+INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
+VALUES ('LB', 'LB', 'Liability', 'Covers medical expenses, lost wages, pain and suffering, and legal fees when the insured driver causes injury or death to others in an accident', 'admin', 'admin');
