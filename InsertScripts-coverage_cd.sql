838ca292b6aa5fb51052997c00b50d197dabe491
Elamparithi-Kannan <e1352457@u.nus.edu>
2024-10-08 19:11:54 +0800

    Update InsertScripts-coverage_cd.sql

--- InsertScripts-coverage_cd.sql
+++ InsertScripts-coverage_cd.sql
@@ -17,19 +17,19 @@ INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME,
 VALUES ('CH', 'FR', 'Fire', 'Comprehensive Coverage: Car Fire', 'admin', 'admin');
 
 INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
-VALUES ('CH', 'FL', 'Comprehensive Coverage: Falling object', 'Falling objects like tree branches', 'admin', 'admin');
+VALUES ('CH', 'FL', 'Falling object', 'Comprehensive Coverage: Falling objects like tree branches', 'admin', 'admin');
 
 INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
-VALUES ('CH', 'AN', 'Comprehensive Coverage: Animal Collision', 'Animal collision like hitting a deer', 'admin', 'admin');
+VALUES ('CH', 'AN', 'Animal Collision', 'Comprehensive Coverage: Animal collision like hitting a deer', 'admin', 'admin');
 
 INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
-VALUES ('CH', 'GL', 'Comprehensive Coverage: Glass Damage', 'Glass Damage like broken windshields', 'admin', 'admin');
+VALUES ('CH', 'GL', 'Glass Damage', 'Comprehensive Coverage: Glass Damage like broken windshields', 'admin', 'admin');
 
 INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
 VALUES ('UN', 'UN', 'Uninsured Motorist', 'Protects the policyholder if they are hit by a driver who does not have insurance. It typically covers medical expenses, lost wages, and other injury-related costs.', 'admin', 'admin');
 
 INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
-VALUES ('MP', 'MP', 'Medical Payments', 'or the health costs of client and the passengers of the client’s vehicle, injured in an accident, regardless of whose fault', 'admin', 'admin');
+VALUES ('MP', 'MP', 'Medical Payments', 'Covers the health costs of client and the passengers of the client’s vehicle, injured in an accident, regardless of whose fault', 'admin', 'admin');
 
 INSERT INTO SYSTEM.COVERAGE_CD (COVERAGE_CODE, SUB_COVERAGE_CODE, COVERAGE_NAME, COVERAGE_DESCRIPTION, CREATED_USER_ID, UPDATED_USER_ID)
 VALUES ('LB', 'LB', 'Liability', 'Covers medical expenses, lost wages, pain and suffering, and legal fees when the insured driver causes injury or death to others in an accident', 'admin', 'admin');
