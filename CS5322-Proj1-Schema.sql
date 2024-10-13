0b252808b78c6f8936071247a85411af53b9846c
ajay-01-bug <114160727+ajay-01-bug@users.noreply.github.com>
2024-10-13 17:28:05 +0530

    Update CS5322-Proj1-Schema.sql

--- CS5322-Proj1-Schema.sql
+++ CS5322-Proj1-Schema.sql
@@ -80,7 +80,7 @@ CREATE TABLE auditor_cd (
 );
 
 CREATE TABLE policy_investigator_cd (
-    investigator_id          NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  -- Unique ID for each investigator
+    investigator_id           NUMBER PRIMARY KEY,  -- Unique ID for each investigator
     investigator_first_name   VARCHAR(100) NOT NULL,       -- First name of the investigator
     investigator_last_name    VARCHAR(100) NOT NULL,       -- Last name of the investigator
     investigator_email        VARCHAR(150) UNIQUE NOT NULL,-- Email of the investigator
@@ -221,7 +221,7 @@ CREATE TABLE claim (
     FOREIGN KEY (coverage_code, sub_coverage_code)    
                             REFERENCES coverage_cd(coverage_code, sub_coverage_code),  -- FK to coverage_cd table
     FOREIGN KEY (auditor_id) REFERENCES auditor_cd(AUDITOR_PK),
-    FOREIGN KEY (policy_inverstigator_id) REFERENCES policy_inverstigator_cd(inverstigator_id)
+    FOREIGN KEY (inverstigator_id) REFERENCES policy_inverstigator_cd(inverstigator_id)
 );
 
 CREATE TABLE payment (
