8fad31c3a8470e971dbbb3abd86e06a641ea9511
Yuntwo <44939876+Yuntwo@users.noreply.github.com>
2024-10-13 19:40:09 +0800

    Update PolicyScripts-policy_holder.sql

--- PolicyScripts-policy_holder.sql
+++ PolicyScripts-policy_holder.sql
@@ -1,3 +1,19 @@
+BEGIN
+  FOR t IN (SELECT table_name FROM all_tables WHERE owner = 'INSURANCE') LOOP
+    EXECUTE IMMEDIATE 'CREATE OR REPLACE TRIGGER trg_set_user_ids_' || t.table_name ||
+      ' BEFORE INSERT OR UPDATE ON ' || t.table_name || 
+      ' FOR EACH ROW
+       BEGIN
+         IF INSERTING THEN
+           :NEW.created_user_id := SYS_CONTEXT('USERENV', ''SESSION_USER'');
+         END IF;
+         IF UPDATING THEN
+           :NEW.updated_user_id := SYS_CONTEXT('USERENV', ''SESSION_USER'');
+         END IF;
+       END;';
+  END LOOP;
+END;
+
 CREATE OR REPLACE FUNCTION policy_holder_vpd_function (p_schema VARCHAR2, p_object VARCHAR2)
 RETURN VARCHAR2 AS
 BEGIN
@@ -11,7 +27,7 @@ BEGIN
     policy_name    => 'policy_holder_select_policy',
     function_schema=> 'INSURANCE',
     policy_function=> 'policy_holder_vpd_function',
-    statement_types=> 'SELECT, INSERT, UPDATE'
+    statement_types=> 'SELECT, UPDATE'
   );
 
   DBMS_RLS.ADD_POLICY(
